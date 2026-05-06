c0cd35bc2dd6ee84b9f50b59bf4108ac0be68b93
VitaoIF <victormelo1212@gmail.com>
2026-05-06 16:33:36 -0300

    feat: adiciona constutor e sobrescrita do metodo atacar

--- untitled/src/entities/Inimigo.java
+++ untitled/src/entities/Inimigo.java
@@ -1,4 +1,19 @@
 package entities;
 
+import java.util.Random;
+
 public class Inimigo extends Personagem{
+
+    public Inimigo() {
+    }
+
+    public Inimigo(Integer carisma, String classe, Integer constituicao, Integer destreza, Integer forca, Integer inteligencia, Integer nivel, String nomePersonagem, String raca, Integer sabedoria) {
+        super(carisma, classe, constituicao, destreza, forca, inteligencia, nivel, nomePersonagem, raca, sabedoria);
+    }
+
+    @Override
+    public int atacar() {
+        return new Random().nextInt(20) + 1 + calcularModificador(getForca());
+    }
+
 }
