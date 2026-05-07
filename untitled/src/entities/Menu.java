d6577b3a2b9520aee779ba5e1f6bf5c6dff46030
VitaoIF <victormelo1212@gmail.com>
2026-05-07 13:57:48 -0300

    feat: adiciona classe menu para iniciar os jogos

---
+++ untitled/src/entities/Menu.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Menu {
+
+    public void criacaoPersonagem(){
+        System.out.println("=- Bem-Vindo a Criação de Personagem -=");
+        Personagem player = new Player();
+
+        System.out.print("Nome do Personagem: ");
+    }
+}
