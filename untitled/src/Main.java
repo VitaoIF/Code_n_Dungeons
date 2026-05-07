aec4f60b5d4f4bbd380736783f1773e1735180cb
VitaoIF <victormelo1212@gmail.com>
2026-05-07 13:57:19 -0300

    testMain: testando a implementação do metodo pontos de habilidades

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,25 +1,14 @@
 import entities.Player;
+import utils.Dados;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        Player player1 = new Player("Dorn", "Monge", 1, "Humano", "Victor", 12, 16, 13, 10, 15, 10);
-        player1.modificadorForca(player1.getForca());
-        player1.modificadorInteligencia(player1.getInteligencia());
-        player1.modificadorDestreza(player1.getDestreza());
-        player1.modificadorConstituicao(player1.getConstituicao());
-        player1.modificadorSabedoria(player1.getSabedoria());
-        player1.modificadorCarisma(player1.getCarisma());
+        int[] total = Dados.pontosDeHabilidades();
 
-        System.out.println(player1.getModificadorForca());
-        System.out.println(player1.getModificadorDestreza());
-        System.out.println(player1.getModificadorConstituicao());
-        System.out.println(player1.getModificadorInteligencia());
-        System.out.println(player1.getModificadorSabedoria());
-        System.out.println(player1.getModificadorCarisma());
-
+        System.out.println(Arrays.toString(total));
     }
 }
